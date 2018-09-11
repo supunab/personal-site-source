9d2b8d92c30c427fe5613bb778f27e60cd959d38
smb564 <supun.14@cse.mrt.ac.lk>
2018-09-12 02:06:43 +0530

    experience component done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router} from '@angular/router';
 
 import {AboutComponent} from './about/about.component';
+import {ExperienceComponent} from './experience/experience.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
-  {path: '', redirectTo: 'about', pathMatch: 'full'}
+  {path: '', redirectTo: 'about', pathMatch: 'full'},
+  {path: 'experience', component: ExperienceComponent}
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
